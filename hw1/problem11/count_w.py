3b7beb89cc1597bbcd80ed6f4338b596b872c032
hortune <b04611015@ntu.edu.tw>
2017-04-08 12:00:20 -0700

    done

--- hw1/problem11/count_w.py
+++ hw1/problem11/count_w.py
@@ -1,5 +1,7 @@
 import sys
+import matplotlib.pyplot as plt
 from math import sqrt
+from pylab import *
 def w_calculate(name):
     f = open(name,'r').read().split('\n')[8:-1]
     w = [0,0]
@@ -10,7 +12,18 @@ def w_calculate(name):
         x2 = float(val[2].split(':')[1])
         w[0] += x1*ay
         w[1] += x2*ay
-    print ("|w| = ",sqrt(w[0]**2+w[1]**2))
+    return sqrt(w[0]**2+w[1]**2)
 
+
+x = [-5,-3,-1,1,3]
+y = []
 for i in range(5):
-    w_calculate('model_'+str(i))
+    y.append(w_calculate('model_'+str(i)))
+
+fig = plt.figure()
+fig.suptitle("|w| in different c",fontsize=20)
+plt.xlabel('C',fontsize = 18)
+plt.ylabel('|W|', fontsize = 16)
+plt.plot([pow(10,i) for i in x],y,color='blue',lw=2)
+plt.xscale('log')
+plt.show()
