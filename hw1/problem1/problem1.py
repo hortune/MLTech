0856d680e797885c7308006ba386d57a2bda0124
hortune <b04611015@ntu.edu.tw>
2017-04-09 01:49:41 -0700

    update problem1

--- hw1/problem1/problem1.py
+++ hw1/problem1/problem1.py
@@ -12,7 +12,12 @@ xpos = [(gen_z1(i[0],i[1]),gen_z2(i[0],i[1])) for (i,j) in zip(x,y) if j == 1 ]
 xneg = [(gen_z1(i[0],i[1]),gen_z2(i[0],i[1])) for (i,j) in zip(x,y) if j == -1 ]
 print (xpos)
 
+fig = plt.figure()
+fig.suptitle("|w| in different c",fontsize=20)
+plt.xlabel('X',fontsize = 18)
+plt.ylabel('Y', fontsize = 16)
 plt.axis([-5,10,-10,5])
 plt.plot([z1 for z1,z2 in xpos ],[z2 for z1,z2 in xpos],'o')
 plt.plot([z1 for z1,z2 in xneg ],[z2 for z1,z2 in xneg],'x')
+plt.plot([4,4,4,4,4],[-10,-2,0,2,5],color="red",lw=2)
 plt.show()
